49770d3fc4f7956b36590a352e888f632fa0d6db
blaisebits <170156795+blaisebits@users.noreply.github.com>
2025-03-22 22:47:51 -0400

    Adding LangGraph studio

--- utils/OutputFormatters.py
+++ utils/OutputFormatters.py
@@ -1,5 +1,6 @@
 from typing import Dict, List
 
+from langchain_core.output_parsers import PydanticOutputParser
 from pydantic import BaseModel, Field
 
 
@@ -13,4 +14,7 @@ class NmapOutputFormat(BaseModel):
     states: List[str] = Field(description="Define if the port is open, closed, or filtered")
     services: List[str] = Field(description="The service running on the associated port")
     versions: List[str] = Field(description="The version of the server software running")
-    scripts: List[List[str]] =Field(description="Nmap Scripting engine output")
+    scripts: List[List[str]] = Field(description="Nmap Scripting engine output")
+
+nmap_parser = PydanticOutputParser(
+    pydantic_object=NmapOutputFormat)
--- utils/Prompts.py
+++ utils/Prompts.py
@@ -3,7 +3,8 @@ from langchain_core.prompts import ChatPromptTemplate
 def get_recon_prompt_template():
     """Recon Agent prompt template to support tool calling
     Takes Tools, Task, Context"""
-    system_template = "You are a cybersecurity expert specializing in network reconnaissance, your primary job is to perform <TASKS> defined below using the provided. Additional <CONTEXT> may be provided and should be taken into consideration."
+    system_template = ("You are an expert cybersecurity agent specializing in network reconnaissance, your primary job is to perform <TASKS> defined below using the provided tooling. Additional <CONTEXT> may be provided and should be taken into consideration."
+                       "Responses should be concise stating only the ACTION to be taken and REASON for the action")
 
 #     <TOOLS>
 # {tools}
